383cc5de47d54f900aafbc0fd3f5bf9072861d74
Andreas Heil <info@aheil.de>
2019-09-20 20:42:43 +0200

    generate 1080p covers

--- Program.cs
+++ Program.cs
@@ -14,23 +14,30 @@ namespace htpcovgen
         static void Main(string[] args)
         {
             var src_path = args[0];
-            var slug = args[1];
-            var episode_no = args[2];
-            var subject = args[3];
+            var scr_path_1080p = args[1];
+            var slug = args[2];
+            var episode_no = args[3];
+            var subject = args[4];
 
             System.IO.Directory.CreateDirectory(OUTPUT_DIR);
 
-            var image = Image.Load(src_path);
 
-            var font = SystemFonts.CreateFont("Consolas", 100, FontStyle.Regular);
-
-            var rendererOptions = new RendererOptions(font);
             var text = "Episode #" + episode_no + " " + '\u00B7' + " " + subject;
 
-            image.Mutate(ctx => ctx.DrawText(text, font, Rgba32.Black, new PointF(100, 2700)));
-
+            // create standard cover
+            var font = SystemFonts.CreateFont("Consolas", 100, FontStyle.Regular);
+            var rendererOptions = new RendererOptions(font);
+            var image = Image.Load(src_path);
+            image.Mutate(ctx => ctx.DrawText(text, font, Rgba32.Black, new PointF(100, 00)));
             image.Save(OUTPUT_DIR + "/" + slug + ".png");
-                
+
+            // create 1080p cover
+            var font_1080p = SystemFonts.CreateFont("Consolas", 50, FontStyle.Regular);
+            var rendererOptions_1080p = new RendererOptions(font_1080p);
+            var image_1080p = Image.Load(scr_path_1080p);
+            image_1080p.Mutate(ctx => ctx.DrawText(text, font_1080p, Rgba32.Black, new PointF(350, 970)));
+            image_1080p.Save(OUTPUT_DIR + "/" + slug + "_1080p.png");
+
         }
     }
 }
